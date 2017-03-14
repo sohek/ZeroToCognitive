000d15e2e32de5cf40eb7dedb7582834a325f0ed
sohek <sohengkiat@Eks-MacBook-Air.local>
2017-03-14 10:42:35 +0800

    Chapter 4

--- Chapter04/controller/restapi/features/speech_to_text.js
+++ Chapter04/controller/restapi/features/speech_to_text.js
@@ -35,7 +35,32 @@ exports.stt_token = function(req, res) {
     });
 }
 
+exports.token = function(req, res) {
+    var sttConfig = extend(config.speech_to_text, vcapServices.getCredentials('speech_to_text'));
+
+    var sttAuthService = watson.authorization(sttConfig);
+
+    sttAuthService.getToken({
+        url: sttConfig.url
+    }, function(err, token) {
+        if (err) {
+            console.log('Error retrieving token: ', err);
+            res.status(500).send('Error retrieving token');
+            return;
+        }
+        res.send(token);
+    });
+}
+
 exports.tts_synthesize = function(req, res) {
   console.log("tts_synthesize entered");
-
+  var ttsConfig = watson.text_to_speech(config.text_to_speech);
+  var transcript = ttsConfig.synthesize(req.query);
+  transcript.on('response', function(response) {
+    if (req.query.download) {
+      response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+    }
+  });
+  transcript.on('error', function(error) { console.log("error encountered: "+error); next(error); });
+  transcript.pipe(res);
 }
